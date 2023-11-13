41bb321dcaf6b015fb2cbbdaf2db982ead47a553
Isaac Sant <isaacjosephsant@gmail.com>
2023-11-12 20:48:33 -0700

    Ionicons not working. Temporarily commented out.

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,14 @@
-import { Text } from 'react-native';
+import { View, Image } from 'react-native';
+
+import Title from '../components/ui/Title';
 
 function GameOverScreen() {
-  return <Text>Game is over!</Text>;
+  return (
+    <View>
+      <Title>GAME OVER!</Title>
+      <Image source={require('../assets/images/success.png')}/>
+    </View>
+  );
 }
 
 export default GameOverScreen;
